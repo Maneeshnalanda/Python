a06d633d0050e099b9cfdc8de12b39f32beb82e7
Maneesh Kumar <maneesh202@gmail.com>
2026-08-17 21:59:08 +0530

    python variable

---
+++ Python variable.py
@@ -0,0 +1,36 @@
+# Python Variables
+
+# A variable is a name used to store a value in memory.
+# Python variables are created when you assign a value to them.
+
+
+# Creating a Variable
+
+name = "maneesh"
+age = 40
+salary = 60000
+
+print(name)
+print(age)
+print(salary)
+
+# Variable with Different Data Types
+
+name = "maneesh"          # String
+age = 40              # Integer
+salary = 60000      # Float
+is_active = True       # Boolean
+
+print(name)
+print(age)
+print(salary)
+print(is_active)
+
+# Assigning Multiple Variables
+
+name, age, city = "maneesh", 40, "Jamshedpur"
+
+print(name)
+print(age)
+print(city)
+
