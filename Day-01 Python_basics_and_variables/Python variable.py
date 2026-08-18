d298a6a663d655e8cd7bc346a56729c30c591790
Maneeshnalanda <maneesh202@gmail.com>
2026-08-18 21:11:19 +0530

    Update Python variable.py

--- Day-01 Python_basics_and_variables/Python variable.py
+++ Day-01 Python_basics_and_variables/Python variable.py
@@ -1,1 +1,55 @@
+# Python Variables
 
+# A variable is a name used to store a value in memory.
+# Python variables are created when you assign a value to them.
+
+
+# Creating a Variable
+
+name = "maneesh"
+age = 40
+salary = 60000
+
+print(name)
+print(age)
+print(salary)
+
+# Variable with Different Data Types
+
+name = "maneesh"          # String
+age = 40              # Integer
+salary = 60000      # Float
+is_active = True       # Boolean
+
+print(name)
+print(age)
+print(salary)
+print(is_active)
+
+# Assigning Multiple Variables
+
+name, age, city = "maneesh", 40, "Jamshedpur"
+
+print(name)
+print(age)
+print(city)
+
+# Assigning the Same Value to Multiple Variables
+
+x = y = z = 100
+
+print(x)
+print(y)
+print(z)
+
+
+
+# Reassigning a Variable
+
+name = "maneesh"
+
+print(name)
+
+name = "Kumar"
+
+print(name)
